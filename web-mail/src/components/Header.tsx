3fb99685152ab1330d89007dcbc60dedc78e5eac
Rostislav Alekseev <mr.rostik.01@gmail.com>
2022-12-17 22:04:59 +0200

    улучшения

--- web-mail/src/components/Header.tsx
+++ web-mail/src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import StyledHeader from "./styles/StyledHeader";
 import FlexContainer from "./styles/FlexContainer";
 import mailLogoWhite from "@/assets/images/mailLogoWhite.svg";
+import { Link } from "react-router-dom";
 
 import LogoSVG from "./styles/svgs/LogoSVG";
 
@@ -9,7 +10,9 @@ function Header(): JSX.Element {
   return (
     <StyledHeader>
       <FlexContainer>
-        <LogoSVG />
+        <Link to={"/inbox"}>
+          <LogoSVG />
+        </Link>
       </FlexContainer>
     </StyledHeader>
   );
