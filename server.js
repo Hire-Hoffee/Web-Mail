3e5ff49e8b519214b3999ddfecf78e5bfc32fbc1
Rostislav Alekseev <mr.rostik.01@gmail.com>
2022-12-18 14:43:25 +0200

    изменение сервера и баг фикс

--- server.js
+++ server.js
@@ -69,34 +69,75 @@ const controllers = {
       throw new Error("Unexpected error occurred");
     }
   },
+  sendStatic: async (req, res) => {
+    let filePath = req.url;
+
+    if (filePath == "/") {
+      filePath = "./index.html";
+    } else {
+      filePath = "." + req.url;
+    }
+
+    let extname = String(path.extname(filePath)).toLowerCase();
+    let mimeTypes = {
+      ".html": "text/html",
+      ".js": "text/javascript",
+      ".css": "text/css",
+      ".json": "application/json",
+      ".png": "image/png",
+      ".jpg": "image/jpg",
+      ".gif": "image/gif",
+      ".svg": "image/svg+xml",
+      ".wav": "audio/wav",
+      ".mp4": "video/mp4",
+      ".woff": "application/font-woff",
+      ".ttf": "application/font-ttf",
+      ".eot": "application/vnd.ms-fontobject",
+      ".otf": "application/font-otf",
+      ".wasm": "application/wasm",
+      ".ico": "image/x-icon",
+    };
+
+    let contentType = mimeTypes[extname] || "application/octet-stream";
+
+    const result = await fs.readFile(filePath);
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(result, "utf-8");
+  },
 };
 
-const requestListener = async function (req, res) {
+const reqListener = async function (req, res) {
   try {
     console.log(`${req.httpVersion} ${req.method} ${req.url}`);
 
     if (req.url.match(/^\/api\/\w+$/) && req.method === "GET") {
       const folder = req.url.split("/")[2];
       controllers.getDataController(req, res, folder);
-    } else if (req.url.match(/\/api\/email\//i) && req.method === "GET") {
+      return;
+    }
+    if (req.url.match(/\/api\/email\//i) && req.method === "GET") {
       const email = req.url.split("/")[3];
       await controllers.getOneMailController(req, res, email);
-    } else {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          message: "Route Not Found: Use the api/[folderName] or api/email/[emailTitle] endpoint",
-        })
-      );
+      return;
     }
+    if (req.url.match(/^\/$/) || (req.url.match(/\/assets\//) && req.method === "GET")) {
+      controllers.sendStatic(req, res);
+      return;
+    }
+
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({ message: "Use the api/[folderName] or api/email/[emailTitle] endpoint" })
+    );
   } catch (error) {
     console.log(error);
+
     res.writeHead(500, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Unexpected error occurred" }));
   }
 };
 
-const server = http.createServer(requestListener);
+const server = http.createServer(reqListener);
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
--- web-mail/src/services/index.ts
+++ web-mail/src/services/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { requestInterceptor, responseInterceptor } from "./interceptors";
 
 const instance = axios.create({
-  baseURL: "/api",
+  baseURL: "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
