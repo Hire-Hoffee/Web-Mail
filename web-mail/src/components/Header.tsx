812a47566a1c283ef349d6c03e55acc9d8bab7e2
Rostislav Alekseev <mr.rostik.01@gmail.com>
2022-12-23 15:56:29 +0200

    bug fix

--- web-mail/src/components/Header.tsx
+++ web-mail/src/components/Header.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import StyledHeader from "./styles/StyledHeader";
 import FlexContainer from "./styles/FlexContainer";
-import { Link, useParams, useNavigate } from "react-router-dom";
+import { Link, useSearchParams, useNavigate } from "react-router-dom";
 
 import LogoSVG from "./styles/svgs/LogoSVG";
 import LeftArrowSVG from "./styles/svgs/LeftArrowSVG";
 
 function Header(): JSX.Element {
-  const { title } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
 
   return (
     <StyledHeader>
       <FlexContainer>
         <Link to={"/inbox"}>
-          {!title ? (
+          {!searchParams.get("title") ? (
             <LogoSVG />
           ) : (
             <div onClick={() => navigate(-1)}>
