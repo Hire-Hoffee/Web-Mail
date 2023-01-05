1be27dd5e5d0706577a23180083285f21c807978
Rostislav Alekseev <mr.rostik.01@gmail.com>
2023-01-05 19:27:17 +0200

    notFound компонент обновлён

--- web-mail/src/components/NotFoundEmails.tsx
+++ web-mail/src/components/NotFoundEmails.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import NotFoundStyles from "./styles/NotFoundStyles";
+import noLetters from "@/assets/images/noLetters.png";
+import noLettersDark from "@/assets/images/noLettersDark.png";
+import { useAppSelector } from "@/store/hooks";
 
 function NotFoundEmails() {
+  const theme = useAppSelector((state) => state.utils.theme);
+
   return (
     <NotFoundStyles>
-      <h1>Письма в данной папке не найдены.</h1>
+      <img src={theme === "white" ? noLetters : noLettersDark} alt="no letters" />
+      <h1>Писем нет</h1>
     </NotFoundStyles>
   );
 }
--- web-mail/src/components/styles/NotFoundStyles.tsx
+++ web-mail/src/components/styles/NotFoundStyles.tsx
@@ -3,12 +3,16 @@ import ThemeType from "@/types/themeType";
 
 const NotFoundStyles = styled.div<{ theme: ThemeType }>`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
   height: 100%;
-  font-size: 20px;
-  font-weight: bold;
+  h1 {
+    margin-top: 20px;
+    font-size: 24px;
+    font-weight: bold;
+  }
 `;
 
 export default NotFoundStyles;
